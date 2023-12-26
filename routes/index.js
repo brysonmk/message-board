47f9788734071ffd85fb12bc5074992af1f0c23f
brysonmk <brysonkushner@gmail.com>
2023-12-26 15:46:04 -0500

    Setup basic message board.

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+
+router.post('/new', function(req, res, next) {
+  const { messageUser, messageText } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
